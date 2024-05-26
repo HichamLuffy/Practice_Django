810e6b66557ebe6bc247d4f0c31bfb2cc1f528c8
HichamLuffy <imalexvssteve2020@gmail.com>
2024-05-26 17:47:02 +0100

    edit urls

--- social_story/core/urls.py
+++ social_story/core/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
 
-    path('main', views.main, name='main'),
+    path('', views.main, name='main'),
     path('signup', views.signup, name='signup'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout')
